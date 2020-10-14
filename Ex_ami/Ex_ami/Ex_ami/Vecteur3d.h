569e0fe8a32c09e8ae02106134c5f969a56ad312
NicolasSemivolos <nicolas67000@free.fr>
2020-10-14 10:13:04 +0200

    Ex_82_fonction_amies
    exercice 82 des fonctions amies

---
+++ Ex_ami/Ex_ami/Ex_ami/Vecteur3d.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class vecteur3d {
+float m_X, m_Y, m_Z; 
+public:  
+	
+	friend int coincide(const vecteur3d&, const vecteur3d&);
+	vecteur3d(float c1 = 0, float c2 = 0, float c3 = 0)
+
+	{
+		m_X = c1; m_Y = c2; m_Zz = c3;
+	};  
+
+
+
--- Ex_ami/Ex_ami/Ex_ami/main.cpp
+++ Ex_ami/Ex_ami/Ex_ami/main.cpp
@@ -17,3 +17,14 @@ int main() {
 
 }
 
+
+// Exercice 82 
+
+#include "vecteur3d.h"   
+
+int coincide (const vecteur3d & v1, const vecteur3d & v2)
+{
+	if ( (v1.m_X == v2.m_X) && (v1.m_Y == v2.m_Y) && (v1.m_Z == v2.m_Z) )    
+		return 1 ;     
+	else return 0 ;}
+
